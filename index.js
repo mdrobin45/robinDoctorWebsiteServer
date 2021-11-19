1b6402affdf0478effcce6c8dc535bbbc1bc4995
Md Robin Rana <robin4500bd@gmail.com>
2021-11-19 12:18:51 +0600

    single service api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
 const express = require('express');
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 const fileUpload = require('express-fileupload');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,6 +37,16 @@ async function insertData()
         app.get('/services', async (req, res) =>
         {
             const cursor = serviceCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Get api for single service
+        app.get('/services/:id', async (req, res) =>
+        {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
